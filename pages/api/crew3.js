ccd99e86c4eca13cf57b0ace17c712a6ad9f88ef
Kevin Compton <kevin@wecreativeagency.com>
2022-11-15 11:13:11 -0800

    supabase setup

--- pages/api/crew3.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// https://api.crew3.xyz/communities/:subdomain/leaderboard/sprint
-// key: 1a3558bTsmngp15DHyWGw4Wj1a3
-
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
