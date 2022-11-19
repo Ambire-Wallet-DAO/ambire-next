4e213f79f7546eb27e167cfcbcdc29fed9f3f4db
Matthew Watman <mattwatman@gmail.com>
2022-11-19 15:09:52 -0500

    Update types for route

--- pages/api/leaderboard.ts
+++ pages/api/leaderboard.ts
@@ -2,10 +2,28 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import initMiddleware from "../../lib/init-middleware";
 import Cors from "cors";
 
+
+
 type LeaderboardData = {
-  name: string
+  leaderboard: User[],
 }
 
+type User = {
+  userId: string,
+  xp: number,
+  name: string,
+  avatar: string,
+  numberOfQuests: number,
+  addresses: {
+    polygon?: string,
+    ethereum?: string,
+    avalanche?: string
+  },
+  address: string,
+  discord: string,
+  twitter: string,
+  discordId: string
+}
 // Initialize the cors middleware
 const cors = initMiddleware(
   Cors({
@@ -14,7 +32,7 @@ const cors = initMiddleware(
   })
 );
 
-export default async function leaderboardHandler(req: NextApiRequest, res: NextApiResponse) {
+export default async function leaderboardHandler(req: NextApiRequest, res: NextApiResponse<LeaderboardData>) {
   await cors(req, res);
   if (req.method != "GET") {
     res.setHeader("Allow", "GET");
@@ -23,7 +41,7 @@ export default async function leaderboardHandler(req: NextApiRequest, res: NextA
   try {
     const response = await fetch("https://api.crew3.xyz/communities/ambire/leaderboard", { method: 'GET', headers: { 'x-api-key': process.env.CREW3_API_KEY } })
     const data = await response.json()
-    res.status(200).json({ message: 'success', data })
+    res.status(200).json(data)
   } catch (err: any) {
     res.setHeader("Allow", "GET");
     res.status(405).end("Method Not Allowed");
