215057b2400212fa284277a4fed89684231ecb13
Matthew Watman <mattwatman@gmail.com>
2022-11-19 14:43:09 -0500

    Add cors, add api route logic to pull crew3 leaderboard data

--- pages/api/leaderboard.ts
+++ pages/api/leaderboard.ts
@@ -1,9 +1,31 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import initMiddleware from "../../lib/init-middleware";
+import Cors from "cors";
 
-type Data = {
+type LeaderboardData = {
   name: string
 }
 
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  res.status(200).json({ name: 'John Doe' })
+// Initialize the cors middleware
+const cors = initMiddleware(
+  Cors({
+    // Only allow requests with GET
+    methods: ["GET"],
+  })
+);
+
+export default async function leaderboardHandler(req: NextApiRequest, res: NextApiResponse) {
+  await cors(req, res);
+  if (req.method != "GET") {
+    res.setHeader("Allow", "GET");
+    res.status(405).end("Method Not Allowed");
+  }
+  try {
+    const response = await fetch("https://api.crew3.xyz/communities/ambire/leaderboard", { method: 'GET', headers: { 'x-api-key': process.env.CREW3_API_KEY } })
+    const data = await response.json()
+    res.status(200).json({ message: 'success', data })
+  } catch (err: any) {
+    res.setHeader("Allow", "GET");
+    res.status(405).end("Method Not Allowed");
+  }
 }
