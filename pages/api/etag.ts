0051fec97886266cc3ef3ae80b2736ea6345d209
Matthew Watman <mattwatman@gmail.com>
2022-11-29 17:25:36 -0500

    Add etag route logic

--- pages/api/etag.ts
+++ pages/api/etag.ts
@@ -13,14 +13,13 @@ const cors = initMiddleware(
 );
 
 
-export default async function testEtagHandler(req: NextApiRequest, res: NextApiResponse) {
+export default async function etagHandler(req: NextApiRequest, res: NextApiResponse) {
   await cors(req, res);
   try {
-    const response1 = await fetch("https://api.crew3.xyz/communities/ambire/leaderboard", { method: 'GET', headers: { 'x-api-key': process.env.CREW3_API_KEY } }) 
+    const response1 = await fetch("https://api.crew3.xyz/communities/ambire/leaderboard", { method: 'GET', headers: { 'x-api-key': process.env.CREW3_API_KEY } })
     const etag = response1.headers.get('Etag')
-    // const response2 = await  fetch("https://api.crew3.xyz/communities/ambire/leaderboard", { method: 'GET', headers: { 'x-api-key': process.env.CREW3_API_KEY, 'If-None-Match': etag } }) 
-    // const response2Data = await response2.json()
-    res.status(200).json({ etag })
+    const response2 = await fetch("https://api.crew3.xyz/communities/ambire/leaderboard", { method: 'GET', headers: { 'x-api-key': process.env.CREW3_API_KEY, 'If-None-Match': "fdakfl;jadkfl;jdakfs;a" } })
+    res.status(response2.status).json({ etag })
   } catch (err: any) {
     res.setHeader("Allow", "GET")
     res.status(405).end("Method Not Allowed")
