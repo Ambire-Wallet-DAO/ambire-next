67cb7b9addfc86df7ecf00620d6bfb1b31920e64
Matthew Watman <mattwatman@gmail.com>
2022-11-29 17:24:58 -0500

    Add route to test etag header value and response statuses

---
+++ pages/api/etag.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import initMiddleware from "../../lib/init-middleware";
+import Cors from "cors";
+
+
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  Cors({
+    // Only allow requests with GET
+    methods: ["GET"],
+  })
+);
+
+
+export default async function testEtagHandler(req: NextApiRequest, res: NextApiResponse) {
+  await cors(req, res);
+  try {
+    const response1 = await fetch("https://api.crew3.xyz/communities/ambire/leaderboard", { method: 'GET', headers: { 'x-api-key': process.env.CREW3_API_KEY } }) 
+    const etag = response1.headers.get('Etag')
+    // const response2 = await  fetch("https://api.crew3.xyz/communities/ambire/leaderboard", { method: 'GET', headers: { 'x-api-key': process.env.CREW3_API_KEY, 'If-None-Match': etag } }) 
+    // const response2Data = await response2.json()
+    res.status(200).json({ etag })
+  } catch (err: any) {
+    res.setHeader("Allow", "GET")
+    res.status(405).end("Method Not Allowed")
+  }
+}
