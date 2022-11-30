e19c20d7c8cfc3920aea576d0a3d9af13efcc715
Matthew Watman <mattwatman@gmail.com>
2022-11-30 10:44:17 -0500

    Chore: remove semicolons and add prettier rule

--- pages/api/leaderboard.ts
+++ pages/api/leaderboard.ts
@@ -1,46 +1,46 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import initMiddleware from "../../lib/init-middleware";
-import Cors from "cors";
+import type { NextApiRequest, NextApiResponse } from "next"
+import initMiddleware from "../../lib/init-middleware"
+import Cors from "cors"
 
 type LeaderboardData = {
-  leaderboard: User[];
-};
+  leaderboard: User[]
+}
 
 type User = {
-  userId: string;
-  xp: number;
-  name: string;
-  avatar: string;
-  numberOfQuests: number;
+  userId: string
+  xp: number
+  name: string
+  avatar: string
+  numberOfQuests: number
   addresses: {
-    polygon?: string;
-    ethereum?: string;
-    avalanche?: string;
-  };
-  address: string;
-  discord?: string;
-  twitter?: string;
-  discordId?: string;
-  roles?: Role[];
-};
+    polygon?: string
+    ethereum?: string
+    avalanche?: string
+  }
+  address: string
+  discord?: string
+  twitter?: string
+  discordId?: string
+  roles?: Role[]
+}
 
 type Role = {
-  id: number;
-  name: string;
-};
+  id: number
+  name: string
+}
 // Initialize the cors middleware
 const cors = initMiddleware(
   Cors({
     // Only allow requests with GET
     methods: ["GET"],
   })
-);
+)
 
 export default async function leaderboardHandler(req: NextApiRequest, res: NextApiResponse) {
-  await cors(req, res);
+  await cors(req, res)
   if (req.method != "GET") {
-    res.setHeader("Allow", "GET");
-    res.status(405).end("Method Not Allowed");
+    res.setHeader("Allow", "GET")
+    res.status(405).end("Method Not Allowed")
   }
   try {
     const [leaderboardData, guildData] = await Promise.all([
@@ -49,14 +49,14 @@ export default async function leaderboardHandler(req: NextApiRequest, res: NextA
         headers: { "x-api-key": process.env.CREW3_API_KEY },
       }),
       fetch("https://api.guild.xyz/v1/guild/the-bean-dao/", { method: "GET" }),
-    ]);
-    const [leaderboardJSON, guildJSON] = await Promise.all([leaderboardData.json(), guildData.json()]);
+    ])
+    const [leaderboardJSON, guildJSON] = await Promise.all([leaderboardData.json(), guildData.json()])
     const roles = guildJSON.roles.map((role) => {
-      return { id: role.id, name: role.name, members: role.members };
-    });
-    res.status(200).json({ ...leaderboardJSON, roles });
+      return { id: role.id, name: role.name, members: role.members }
+    })
+    res.status(200).json({ ...leaderboardJSON, roles })
   } catch (err: any) {
-    res.setHeader("Allow", "GET");
-    res.status(405).end("Method Not Allowed");
+    res.setHeader("Allow", "GET")
+    res.status(405).end("Method Not Allowed")
   }
 }
