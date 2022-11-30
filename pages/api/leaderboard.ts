234139723e919fc6dcd91697820fad5cf511ed38
Matthew Watman <mattwatman@gmail.com>
2022-11-30 10:25:02 -0500

    Chore: test formatting

--- pages/api/leaderboard.ts
+++ pages/api/leaderboard.ts
@@ -1,34 +1,33 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 import initMiddleware from "../../lib/init-middleware";
 import Cors from "cors";
 
-
 type LeaderboardData = {
-  leaderboard: User[],
-}
+  leaderboard: User[];
+};
 
 type User = {
-  userId: string,
-  xp: number,
-  name: string,
-  avatar: string,
-  numberOfQuests: number,
+  userId: string;
+  xp: number;
+  name: string;
+  avatar: string;
+  numberOfQuests: number;
   addresses: {
-    polygon?: string,
-    ethereum?: string,
-    avalanche?: string
-  },
-  address: string,
-  discord?: string,
-  twitter?: string,
-  discordId?: string,
-  roles?: Role[]
-}
+    polygon?: string;
+    ethereum?: string;
+    avalanche?: string;
+  };
+  address: string;
+  discord?: string;
+  twitter?: string;
+  discordId?: string;
+  roles?: Role[];
+};
 
 type Role = {
-  id: number,
-  name: string,
-}
+  id: number;
+  name: string;
+};
 // Initialize the cors middleware
 const cors = initMiddleware(
   Cors({
@@ -45,14 +44,19 @@ export default async function leaderboardHandler(req: NextApiRequest, res: NextA
   }
   try {
     const [leaderboardData, guildData] = await Promise.all([
-      fetch("https://api.crew3.xyz/communities/ambire/leaderboard", { method: 'GET', headers: { 'x-api-key': process.env.CREW3_API_KEY } }),
-      fetch("https://api.guild.xyz/v1/guild/the-bean-dao/", { method: 'GET' })
-    ])
-    const [leaderboardJSON, guildJSON] = await Promise.all([leaderboardData.json(), guildData.json()])
-    const roles = guildJSON.roles.map(role => { return { id: role.id, name: role.name, members: role.members } })
-    res.status(200).json({ ...leaderboardJSON, roles })
+      fetch("https://api.crew3.xyz/communities/ambire/leaderboard", {
+        method: "GET",
+        headers: { "x-api-key": process.env.CREW3_API_KEY },
+      }),
+      fetch("https://api.guild.xyz/v1/guild/the-bean-dao/", { method: "GET" }),
+    ]);
+    const [leaderboardJSON, guildJSON] = await Promise.all([leaderboardData.json(), guildData.json()]);
+    const roles = guildJSON.roles.map((role) => {
+      return { id: role.id, name: role.name, members: role.members };
+    });
+    res.status(200).json({ ...leaderboardJSON, roles });
   } catch (err: any) {
-    res.setHeader("Allow", "GET")
-    res.status(405).end("Method Not Allowed")
+    res.setHeader("Allow", "GET");
+    res.status(405).end("Method Not Allowed");
   }
 }
