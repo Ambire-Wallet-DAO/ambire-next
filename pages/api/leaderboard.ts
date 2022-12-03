83f3a69c0a19c27967c1a6fe98cba379e2c4a86e
Matthew Watman <mattwatman@gmail.com>
2022-12-02 20:45:56 -0500

    Turn off semicolons and update quotes to single

--- pages/api/leaderboard.ts
+++ pages/api/leaderboard.ts
@@ -1,6 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-import initMiddleware from "../../lib/init-middleware"
-import Cors from "cors"
+import type { NextApiRequest, NextApiResponse } from 'next'
+import initMiddleware from '../../lib/init-middleware'
+import Cors from 'cors'
 
 type LeaderboardData = {
   leaderboard: User[]
@@ -32,23 +32,23 @@ type Role = {
 const cors = initMiddleware(
   Cors({
     // Only allow requests with GET
-    methods: ["GET"],
+    methods: ['GET'],
   })
 )
 
 export default async function leaderboardHandler(req: NextApiRequest, res: NextApiResponse) {
   await cors(req, res)
-  if (req.method != "GET") {
-    res.setHeader("Allow", "GET")
-    res.status(405).end("Method Not Allowed")
+  if (req.method != 'GET') {
+    res.setHeader('Allow', 'GET')
+    res.status(405).end('Method Not Allowed')
   }
   try {
     const [leaderboardData, guildData] = await Promise.all([
-      fetch("https://api.crew3.xyz/communities/ambire/leaderboard", {
-        method: "GET",
-        headers: { "x-api-key": process.env.CREW3_API_KEY },
+      fetch('https://api.crew3.xyz/communities/ambire/leaderboard', {
+        method: 'GET',
+        headers: { 'x-api-key': process.env.CREW3_API_KEY },
       }),
-      fetch("https://api.guild.xyz/v1/guild/the-bean-dao/", { method: "GET" }),
+      fetch('https://api.guild.xyz/v1/guild/the-bean-dao/', { method: 'GET' }),
     ])
     const [leaderboardJSON, guildJSON] = await Promise.all([leaderboardData.json(), guildData.json()])
     const roles = guildJSON.roles.map((role) => {
@@ -56,7 +56,7 @@ export default async function leaderboardHandler(req: NextApiRequest, res: NextA
     })
     res.status(200).json({ ...leaderboardJSON, roles })
   } catch (err: any) {
-    res.setHeader("Allow", "GET")
-    res.status(405).end("Method Not Allowed")
+    res.setHeader('Allow', 'GET')
+    res.status(405).end('Method Not Allowed')
   }
 }
