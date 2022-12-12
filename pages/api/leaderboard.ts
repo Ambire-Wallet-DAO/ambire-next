b04282198485df4d2e4d0d3787bc5f9101b696dc
Matthew Watman <mattwatman@gmail.com>
2022-12-12 13:48:52 -0500

    feat(api): switch fetch to supabase instead of crew3 directly

--- pages/api/leaderboard.ts
+++ pages/api/leaderboard.ts
@@ -1,33 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { supabase } from '../../lib/initSupabase'
 import initMiddleware from '../../lib/init-middleware'
 import Cors from 'cors'
 
-type LeaderboardData = {
-  leaderboard: User[]
-}
-
-type User = {
-  userId: string
-  xp: number
-  name: string
-  avatar: string
-  numberOfQuests: number
-  addresses: {
-    polygon?: string
-    ethereum?: string
-    avalanche?: string
-  }
-  address: string
-  discord?: string
-  twitter?: string
-  discordId?: string
-  roles?: Role[]
-}
-
-type Role = {
-  id: number
-  name: string
-}
 // Initialize the cors middleware
 const cors = initMiddleware(
   Cors({
@@ -38,25 +13,11 @@ const cors = initMiddleware(
 
 export default async function leaderboardHandler(req: NextApiRequest, res: NextApiResponse) {
   await cors(req, res)
-  if (req.method != 'GET') {
-    res.setHeader('Allow', 'GET')
-    res.status(405).end('Method Not Allowed')
-  }
   try {
-    const [leaderboardData, guildData] = await Promise.all([
-      fetch('https://api.crew3.xyz/communities/ambire/leaderboard', {
-        method: 'GET',
-        headers: { 'x-api-key': process.env.CREW3_API_KEY },
-      }),
-      fetch('https://api.guild.xyz/v1/guild/the-bean-dao/', { method: 'GET' }),
-    ])
-    const [leaderboardJSON, guildJSON] = await Promise.all([leaderboardData.json(), guildData.json()])
-    const roles = guildJSON.roles.map((role) => {
-      return { id: role.id, name: role.name, members: role.members }
-    })
-    res.status(200).json({ ...leaderboardJSON, roles })
+    const { data, error } = await supabase.from('Users').select('name, xp, numberOfQuests, roles("*")')
+    if (error) throw Error(error.message)
+    res.status(200).json(data)
   } catch (err: any) {
-    res.setHeader('Allow', 'GET')
-    res.status(405).end('Method Not Allowed')
+    res.status(401).json(err.message)
   }
 }
