9763ab049f3d246043ed0a72fbebb1513bd3bb9f
Matthew Watman <mattwatman@gmail.com>
2022-12-13 15:15:36 -0500

    feat(api): update etag logic

--- pages/api/refresh.ts
+++ pages/api/refresh.ts
@@ -14,24 +14,23 @@ const cors = initMiddleware(
 export default async function refreshDatabase(req: NextApiRequest, res: NextApiResponse) {
   await cors(req, res)
   try {
-    const etagResponse = await fetch('https://api.crew3.xyz/communities/ambire/leaderboard', {
-      method: 'GET',
-      headers: { 'x-api-key': process.env.CREW3_API_KEY },
-    })
-    const etag = etagResponse.headers.get('etag')
+    const { data, error } = await supabase.from('Etag').select('*')
+    if (error) throw new Error(error.message)
+    const current_etag = data[0].current_value
+    const etagId = data[0].id
+
     const leaderboardResponse = await fetch('https://api.crew3.xyz/communities/ambire/leaderboard', {
       method: 'GET',
-      headers: { 'x-api-key': process.env.CREW3_API_KEY, 'If-None-Match': etag },
+      headers: { 'x-api-key': process.env.CREW3_API_KEY, 'If-None-Match': current_etag },
     })
     if (leaderboardResponse.status == 200) {
       const etag2 = leaderboardResponse.headers.get('etag')
-      // const { data, error } = await supabase.from('Etag').upsert({ current_value: etag2 }).select()
-      // if (error) throw Error(error.message)
-      res.status(200).json({ etag, etag2 })
+      const { data, error } = await supabase.from('Etag').update({ current_value: etag2 }).eq('id', etagId).select()
+      if (error) throw Error(error.message)
+      res.status(200).json(data)
     } else {
       res.status(leaderboardResponse.status).json({})
     }
-    res.status(200).json({ etag })
   } catch (err: any) {
     res.status(401).json({ error: err.message })
   }
